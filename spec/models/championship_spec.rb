9f03f68c5630fcd6930557775681e15da2ded509
Henrique Okomura <henrique.okomura@gmail.com>
2017-06-24 20:51:35 +0000

    Specs for bracket generation

--- spec/models/championship_spec.rb
+++ spec/models/championship_spec.rb
@@ -2,6 +2,50 @@ require 'rails_helper'
 
 describe Championship do
   
+  describe '#generete_brackets!' do
+    it 'raises an Championship::InvalidParticipantsNumber if the number of participants is not power of 2' do
+      subject.participants = FactoryGirl.build_list :participant, 3
+      expect { subject.generete_brackets! }.to raise_error Championship::InvalidParticipantsNumber
+    end
+    
+    it 'raises an Championship::InvalidChampionshipType if the number of participants is not power of 2' do
+      allow(subject).to receive(:brackets?).and_return(false)
+      expect { subject.generete_brackets! }.to raise_error Championship::InvalidChampionshipType
+    end
+    
+    it 'generates the brackets correctly' do
+      subject.participants = FactoryGirl.build_list :participant, 8
+      brackets = subject.generete_brackets!
+      
+      expect(brackets.count).to eq 7
+      brackets.each do |bracket|
+        expect(bracket.championship_id).to eq subject.id
+      end
+      
+      filled_brackets = brackets.select { |bracket| bracket.player_1.present? }
+      expect(filled_brackets.count).to eq 4
+      filled_brackets.each_with_object([]) do |bracket, players_in_matches|
+        expect(bracket.parent).to be_present
+        
+        expect(bracket.player_1).to be_in subject.participants
+        expect(players_in_matches).not_to include(bracket.player_1)
+        players_in_matches << bracket.player_1
+        
+        expect(bracket.player_2).to be_in subject.participants
+        expect(players_in_matches).not_to include(bracket.player_2)
+        players_in_matches << bracket.player_2
+      end
+
+      finals = brackets.select { |bracket| bracket.parent.blank? }
+      expect(finals.count).to eq 1
+      
+      brackets.reject { |bracket| bracket.in?(finals) }.each do |not_final_bracket|
+        children = brackets.select { |bracket| bracket.parent == not_final_bracket}
+        expect(children.count).to eq 2
+      end
+    end
+  end
+  
   describe '#full_loaded_final' do
     subject { FactoryGirl.create :championship }
     let!(:root) { FactoryGirl.create :bracket, championship: subject }
