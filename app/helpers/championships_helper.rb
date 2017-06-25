1c6d765c09a34b583152af6d2bbe789a7b130ab4
Carolina Tiemi Fernandes Arashiro <tiemiarashiro@gmail.com>
2017-06-24 22:49:52 -0300

    View brackets tree finished and created links to define brackets winners

--- app/helpers/championships_helper.rb
+++ app/helpers/championships_helper.rb
@@ -1,22 +1,35 @@
 module ChampionshipsHelper
 
   def render_brackets(championship)
-    html = "<ul>";
-    html += render_node(championship.full_loaded_final, html)
-    html += "</ul>";
+    html = "<ul>"
+    html += render_node(championship.full_loaded_final)
+    html += "</ul>"
+    html.html_safe
   end
 
-  def render_node(node, html)
-    html += "<li>"
-    html += "<a>"+node.player1.user.name+" x "+node.player2.user.name+"</a>"
-    if (node.children.count > 0)
-      html +=  "<ul>";
-      node.children.each do |child|
-        render_node(child, html);
+  def render_node(node)
+    html = "<li>"
+    if(node.player_1.present? && node.player_1.present?)
+      if(node.finished?)
+        html += "<span>"+node.player_1.user.name+" x "+node.player_2.user.name+"</span>"
+      else
+        html += "<span>"
+        html += "<a href='#'>"+node.player_1.user.name+"</a>"
+        html += " x "
+        html += "<a href='#'>"+node.player_2.user.name+"</a>"
+        html += "</span>"
       end
-      html +=  "</ul>";
+    else
+      html += "<span>? x ?</span>"
     end
-    html +=  "</li>";
+    if (node.children.count > 0)
+      html +=  "<ul>"
+      node.children.each do |child|
+        html += render_node(child)
+      end
+      html += "</ul>"
+    end
+    html += "</li>"
   end
 
 end
--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,14 +1,18 @@
 class Bracket < ApplicationRecord
   attr_accessor :children
-  
+
   belongs_to :championship
   belongs_to :parent, class_name: 'Bracket', required: false
   belongs_to :winner, class_name: 'Participant', required: false
   belongs_to :player_1, class_name: 'Participant', required: false
   belongs_to :player_2, class_name: 'Participant', required: false
-  
+
   def children
     @children ||= Bracket.where(parent_id: self.id)
+  end
+
+  def finished?
+    self.winner.present?
   end
 
 end
