df67e2bf914a055610a9996aee31b7cc1855b946
Henrique Okomura <henrique.okomura@codus.com.br>
2017-11-14 16:29:50 -0200

    Filtro case insensitive

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -2,10 +2,10 @@ class Interest < ApplicationRecord
   belongs_to :user
 
   scope :by_format, -> (format) do
-    where('format LIKE ?', "%#{format}%")
+    where('lower(format) LIKE ?', "%#{format.downcase}%")
   end
 
   scope :by_local, -> (local) do
-    where('local LIKE ?', "%#{local}%")
+    where('lower(local) LIKE ?', "%#{local.downcase}%")
   end
 end
